4eab3a21ca47850f748a72d8b5bd1ee98feee47b
Stewart Chandler <stewartdchandler@gmail.com>
2025-12-22 12:50:34 -0500

    basic lean nocrt windows app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::{ffi::c_int, panic::PanicInfo};
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "system" fn _start() -> c_int {
+    0
 }
